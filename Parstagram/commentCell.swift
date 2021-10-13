5223dba6f937dcff560c0f89eec7f705280a9b5f
Yipeng <Yipengg@gmail.com>
2021-10-13 01:14:28 -0400

    last

---
+++ Parstagram/commentCell.swift
@@ -0,0 +1,29 @@
+//
+//  commentCell.swift
+//  Parstagram
+//
+//  Created by yipeng li on 10/12/21.
+//
+
+import UIKit
+
+class commentCell: UITableViewCell {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var commentLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    
+
+}
